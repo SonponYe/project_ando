1c1347d34463eb1456847cec4e2fba4258b25563
SonponYe-shuaChief <sonponyeshua@gmail.com>
2026-05-07 14:16:37 +0000

    works now i thinkfixes borwser router in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // src/index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./App.css";
 
